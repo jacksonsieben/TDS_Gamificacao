4767b261f96728632567c5c1ae712b3f2183665e
Jackson Sieben <jackson_sieben@live.com>
2023-04-10 09:55:04 -0300

    LIST GARCON

--- ProjetoGerenciamentoRestaurante.RazorPages/Models/AtendimentoModel.cs
+++ ProjetoGerenciamentoRestaurante.RazorPages/Models/AtendimentoModel.cs
@@ -5,8 +5,8 @@ namespace ProjetoGerenciamentoRestaurante.RazorPages.Models
         public int Id { get; set; }
         public int Id_Mesa { get; set; }
         public MesaModel? Mesa { get; set; }
-        public int Id_Garcon { get; set; }
-        public GarconModel? Garcon { get; set; }
+        //public int Id_Garcon { get; set; }
+        public List<GarconModel>? Garcons { get; set; } //mudei para Lista de garcons pois uma mesa pode ser atendida por varios garcons 
         public DateTime HorarioPedido { get; set; }
         public List<ProdutoModel>? Produtos { get; set; }
     }

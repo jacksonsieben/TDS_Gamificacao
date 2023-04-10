89ba29369cfeeedc23b8b5fa2fca18056d04b0db
rodrignucleo <rodrignucleo@gmail.com>
2023-04-10 09:26:47 -0300

    add razor pages

--- ProjetoGerenciamentoRestaurante.RazorPages/Program.cs
+++ ProjetoGerenciamentoRestaurante.RazorPages/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapRazorPages();
 
 app.Run();
